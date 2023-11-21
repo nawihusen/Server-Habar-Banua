ae0ad2bc3ac6435b56aa242cab0ee3ddd8fac8e5
nawihusen <nawihusen@gmail.com>
2023-11-21 22:16:46 +0800

    tambah

--- src/main/java/com/habarbanua/utils/Helper.java
+++ src/main/java/com/habarbanua/utils/Helper.java
@@ -1,9 +1,18 @@
 package com.habarbanua.utils;
 
+import java.time.Instant;
+
 public class Helper {
 
     static String sebuahHelper(){
         return "";
     }
 
+    static String toStringInstant(Instant instant){
+        return "";
+    }
+
+    static Instant toInstantString(String string){
+        return null;
+    }
 }
